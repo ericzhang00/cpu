fad731c433d3410fba5d9db78a12211d8b1a569f
Mae Wang <maewang@berkeley.edu>
2019-11-19 01:29:54 -0800

    rtype added

--- r_type.s
+++ r_type.s
@@ -60,8 +60,131 @@ sll x10 x10 x6
 sll x10 x0 x0
 sll x0 x10 x5
 
+addi x5 x0 1 #x5 = 1
+addi x6 x0 -1 #x6 = -1
+addi x7 x0 2 #x7 = 2
+mulh x0 x5 x6
+mulh x2 x0 x6
+mulh x2 x5 x6
+mulh x1 x5 x5
+mulh x1 x5 x7
+mulh x1 x6 x6
+mulh x1 x6 x1
 
+addi x5 x0 2 #x5 = 2
+addi x6 x0 -2 #x6 = -2
+addi x7 x0 4 #x7 = 4
+mulhu x0 x5 x6
+mulhu x2 x0 x6
+mulhu x2 x5 x6
+mulhu x1 x5 x5
+mulhu x1 x5 x7
+mulhu x1 x6 x6
+mulhu x1 x6 x1
 
+addi x5 x0 2 #x5 = 2
+addi x6 x0 -2 #x6 = -2
+addi x7 x0 4 #x7 = 4
+slt x10 x5 x5
+slt x10 x5 x0
+slt x10 x0 x5
+slt x10 x6 x5
+slt x10 x5 x6
+slt x10 x5 x7
+slt x10 x7 x5
+
+addi x5 x0 2 #x5 = 2
+addi x6 x0 -2 #x6 = -2
+addi x7 x0 4 #x7 = 4
+xor x9 x5 x5
+xor x9 x5 x0
+xor x9 x0 x5
+xor x9 x6 x5
+xor x9 x5 x6
+xor x9 x5 x7
+xor x10 x7 x5
+
+addi x6 x5 -120
+addi x7 x0 5 
+addi x10 x0 -2
+divu x1 x6 x7 
+divu x8 x1 x10 
+divu x1 x0 x7 
+divu x0 x8 x6 
+divu x8 x8 x6 
+divu x8 x8 x8 
+divu x8 x8 x8 
+divu x1 x1 x0 
+divu x1 x6 x0 
+
+addi x5 x0 1 
+addi x6 x0 -1 
+addi x7 x0 2 
+srl x9 x0 x5 
+srl x10 x7 x5 
+srl x10 x10 x5 
+srl x10 x10 x6 
+addi x5 x0 8 
+srl x10 x10 x5 
+srl x10 x10 x5
+srl x10 x10 x5
+srl x10 x10 x5 
+srl x10 x10 x5
+srl x10 x10 x5
+srl x10 x10 x5
+srl x10 x10 x5
+srl x10 x10 x5
+srl x10 x10 x5
+srl x10 x10 x5
+addi x6 x0 -8 
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x10 x6
+srl x10 x0 x0
+srl x0 x10 x5
+
+addi x6 x5 -120
+addi x7 x0 5 
+addi x10 x0 -2
+remu x1 x6 x7 
+remu x8 x1 x10 
+remu x1 x0 x7 
+remu x0 x8 x6 
+remu x8 x8 x6 
+remu x8 x8 x8 
+remu x8 x8 x8 
+remu x1 x1 x0 
+remu x1 x6 x0 
+
+addi x5 x0 2 #x5 = 2
+addi x6 x0 -2 #x6 = -2
+addi x7 x0 4 #x7 = 4
+and x9 x5 x5
+and x9 x5 x0
+and x9 x0 x5
+and x9 x6 x5
+and x9 x5 x6
+and x9 x5 x7
+and x10 x7 x5
+
+addi x5 x0 2 #x5 = 2
+addi x6 x0 -2 #x6 = -2
+addi x7 x0 4 #x7 = 4
+or x8 x5 x5
+or x8 x5 x0
+or x8 x0 x5
+or x8 x6 x5
+or x8 x5 x6
+or x8 x5 x7
+or x8 x7 x5
 
 
 
