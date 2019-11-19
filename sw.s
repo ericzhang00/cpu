822b95fe94595a424c18f8e57912531bac555316
Mae Wang <maewang@berkeley.edu>
2019-11-18 23:12:01 -0800

    added .s files

--- sw.s
+++ sw.s
@@ -1,10 +1,16 @@
 addi x1 x0 1
 addi x2 x0 14
 sw x1 0(x5)
+lw x8 0(x5)
 sw x2 4(x6)
+lh x9 4(x6)
+lw x10 4(x6)
 add x1 x1 x1
 sw x1 8(x7)
+lb x2 8(x7)
+lw x2 8(x7)
 add x2 x2 x7
 sw x2 16(x1)
+lb x1 16(x1)
 add x7 x2 x2
 sw x7 32(x7)
