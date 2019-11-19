87a8c4b743b38cefb4d9693f51d549f69a218e82
Mae Wang <maewang@berkeley.edu>
2019-11-19 02:52:26 -0800

    add edgecase

---
+++ edgecase.s
@@ -0,0 +1,17 @@
+addi x10 x0 5
+addi x8 x0 -61
+lui x7 1234
+lui x6 0
+
+loop:
+beq x10 x9 end0
+addi x9 x9 1
+jal x1 dothis
+blt x0 x9 loop
+
+dothis:
+jalr x1 x0 0
+
+end0:
+add x0 x0 x0
+#lui x7 -61 #should fail
