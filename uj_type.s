6cc70548fe2127e006f2cfa930cf82d4d79d73f2
Mae Wang <maewang@berkeley.edu>
2019-11-19 02:36:16 -0800

    ujutype added

--- uj_type.s
+++ uj_type.s
@@ -1,20 +1,20 @@
-addi x10 x0 5
-addi x8 x0 -61
-lui x7 1234
-lui x6 0
+# addi x10 x0 5
+# addi x8 x0 -61
+# lui x7 1234
+# lui x6 0
 
-loop:
-beq x10 x9 end0
-addi x9 x9 1
-jal x1 -24
-blt x0 x9 loop
+# loop:
+# beq x10 x9 end0
+# addi x9 x9 1
+# jal x1 -24
+# blt x0 x9 loop
 
 # neverdothis:
 # jal x1 0
 
-end0:
-add x0 x0 x0
-jal x0 4
+# end0:
+# add x0 x0 x0
+# jal x0 4
 
 
 
